11339afb68f230a9e67863ec168edf9627e83756
sinem <sinem.kaya2018gmail.com>
2022-04-10 18:45:48 -0500

    Hook class is created and isTakeAScreenshot annotation added

---
+++ src/test/java/com/trycloud/stepDefinitions/Hook.java
@@ -0,0 +1,28 @@
+package com.trycloud.stepDefinitions;
+
+import com.trycloud.utilities.utilities.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+public class Hook {
+    @After
+    public void teardownScenario(Scenario scenario){
+
+        //scenario.isFailed() --> if scenario fails this method will return TRUE boolean value
+
+        if (scenario.isFailed()){
+
+            byte [] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", scenario.getName());
+
+        }
+
+
+        Driver.closeDriver();
+        //BrowserUtils.sleep(5);
+
+    }
+
+}
