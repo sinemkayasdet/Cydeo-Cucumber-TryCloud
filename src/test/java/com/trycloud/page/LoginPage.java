ba8de8486983b89e138889505e9548a4686cbc21
sinem <sinem.kaya2018gmail.com>
2022-04-10 18:46:26 -0500

    Login page is created

--- src/test/java/com/trycloud/page/LoginPage.java
+++ src/test/java/com/trycloud/page/LoginPage.java
@@ -1,6 +1,10 @@
 package com.trycloud.page;
 
+import com.trycloud.utilities.utilities.ConfigurationReader;
 import com.trycloud.utilities.utilities.Driver;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
@@ -8,6 +12,44 @@ public class LoginPage {
     public LoginPage(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
+
+    @FindBy(id = "user")
+    public WebElement userInput;
+
+    @FindBy(id = "password")
+    public WebElement passwordInput;
+
+    @FindBy(id = "submit-form")
+    public WebElement submitFormInput;
+
+    @FindBy(id = "lost-password")
+    public WebElement lostpasswordLink;
+
+    @FindBy(xpath = "//a[normalize-space(text()) = 'Log in with a device']")
+    public WebElement elementLink;
+
+    @FindBy(css = ".info")
+    public WebElement infoP;
+
+    @FindBy(xpath = "//p[@class='warning wrongPasswordMsg']")
+    public WebElement warningMessage;
+
+
+
+    public void loginBy(String username, String password) {
+        userInput.sendKeys(username);
+        passwordInput.sendKeys(password);
+       // submitFormInput.click();
+    }
+
+    public void loginBy() {
+        userInput.sendKeys(ConfigurationReader.getProperty("username"));
+        passwordInput.sendKeys(ConfigurationReader.getProperty("password"));
+        submitFormInput.click();
+    }
+
+
+
 
 
 }
